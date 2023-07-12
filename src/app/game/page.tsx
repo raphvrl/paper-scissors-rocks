702b99ac1145c45633d2da9058ac39dfc222cc8d
sayuze145 <raphael.verrouil@epitech.eu>
2023-07-12 21:21:19 +0200

    refactor: change a comment

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -28,7 +28,7 @@ export default function Game() {
     const [playerChoice, setPlayerChoice] = useState("null")
     const [computerChoice, setComputerChoice] = useState("null")
 
-    // verif a the player score is better than his best score
+    // verif the player's score is better than his best score
     useEffect(() => {
         if (playerName === null || strLeaderboard === null)
             return
